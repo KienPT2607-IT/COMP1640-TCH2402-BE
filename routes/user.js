d81ac875a4284d452e0a0c348c657ba2d6dc3196
truongpm265 <truongpm265@gmail.com>
2024-03-04 13:42:29 +0700

    first commit

--- routes/user.js
+++ routes/user.js
@@ -8,20 +8,72 @@ const UserModel = require("../models/UserModel");
 router.get("/", async (req, res) => {
 	try {
 		let users = await UserModel.find();
+		console.log(users.length);
+
 		res.status(200).json({
 			data: users,
 		});
 	} catch (error) {
 		console.log("Found an error:" + error);
-		res.status(401).json({
-			error: error,
+		res.status(404).json({
+			error: error.message,
 		});
 	}
 });
 
+router.post("/login", async (req, res) => {
+	try {
+		const { password, email } = req.body;
+		console.log(email + " / " + password);
+		let user = await UserModel.findOne({
+			email: email,
+			password: password,
+		});
+		console.log(user);
+		res.status(200).json({
+			data: user,
+		});
+	} catch (error) {
+		console.log("Found an error:" + error);
+		res.status(404).json({
+			error: error.message,
+		});
+	}
+});
 
-
-
-
-
+router.post("/register", async (req, res) => {
+	try {
+		const {
+			full_name,
+			email,
+			dob,
+			gender,
+			phone_number,
+			profile_picture,
+			created_by,
+			role,
+		} = req.body;
+		let mngCoordinator = await UserModel.findById(created_by);
+		await UserModel.create({
+			full_name: full_name,
+			email: email,
+			dob: dob,
+			phone_number: phone_number,
+			gender: gender,
+			profile_picture: profile_picture,
+			registration_date: Date.now,
+			faculty: mngCoordinator.faculty,
+			created_by: created_by,
+			role: role,
+		});
+		res.status(201).json({
+			message: "Student added successfully!"
+		})
+	} catch (error) {
+		console.log("Found an error:" + error);
+		res.status(404).json({
+			error: error.message,
+		});
+	}
+});
 module.exports = router;
